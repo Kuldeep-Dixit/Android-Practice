b36b789c9c4ff7c341f30a3de68e9fdf8d482a26
Kuldeep Dixit <dixitk488@gmail.com>
2025-03-22 17:14:11 +0530

    Using Static Fragments

---
+++ FragmentsExample/app/src/main/java/com/example/fragmentsexample/FragmentA.java
@@ -0,0 +1,28 @@
+package com.example.fragmentsexample;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentA extends Fragment {
+
+
+    public FragmentA() {
+        // Required empty public constructor
+    }
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_a, container, false);
+         TextView txtView = view.findViewById(R.id.fragA);
+        return view;
+    }
+}
---
+++ FragmentsExample/app/src/main/java/com/example/fragmentsexample/FragmentB.java
@@ -0,0 +1,30 @@
+package com.example.fragmentsexample;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class FragmentB extends Fragment {
+
+
+    public FragmentB() {
+        // Required empty public constructor
+    }
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_b, container, false);
+        TextView txtView = view.findViewById(R.id.fragB);
+        return view;
+    }
+}
