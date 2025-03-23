12b4d66177b11e474701af82dbaefece8ac7a81f
Kuldeep Dixit <dixitk488@gmail.com>
2025-03-23 09:48:46 +0530

    adding dynamic fragment

--- FragmentsExample/app/src/main/java/com/example/fragmentsexample/MainActivity.java
+++ FragmentsExample/app/src/main/java/com/example/fragmentsexample/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.fragmentsexample;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnA,btnB,btnC;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnA = findViewById(R.id.btnfragA);
+        btnB = findViewById(R.id.btnfragB);
+        btnC = findViewById(R.id.btnfragC);
+
+        loadFrag(new FragmentB(),0);
+
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFrag(new FragmentA(),1);
+            }
+        });
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFrag(new FragmentB(),1);
+            }
+        });
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFrag(new FragmentC(),1);
+            }
+        });
+    }
+    public void loadFrag(Fragment fragment,int flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if(flag==0)
+            ft.add(R.id.lyContainer,fragment);
+        else
+            ft.replace(R.id.lyContainer,fragment);
+
+        ft.commit();
+
     }
 }
