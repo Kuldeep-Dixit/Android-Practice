87bcc8753f5ab91049df3a54f31d0b918c85a287
Kuldeep Dixit <dixitk488@gmail.com>
2025-03-23 09:57:14 +0530

    Implicit, Explicit Intent passing using call, message, email and share button

--- ImplicitExplicitIntentPassing/app/src/main/java/com/example/implicitexplicitintentpassing/MainActivity.java
+++ ImplicitExplicitIntentPassing/app/src/main/java/com/example/implicitexplicitintentpassing/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.implicitexplicitintentpassing;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnDial,btnMsg,btnEmail,btnShare;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,52 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnDial=findViewById(R.id.btnDial);
+        btnEmail=findViewById(R.id.btnEmail);
+        btnMsg = findViewById(R.id.btnMsg);
+        btnShare = findViewById(R.id.btnShare);
+
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iDial = new Intent(Intent.ACTION_DIAL);
+                iDial.setData(Uri.parse("tel: +917902098062"));
+                startActivity(iDial);
+            }
+        });
+        btnMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent iMsg = new Intent(Intent.ACTION_SENDTO);
+                iMsg.setData(Uri.parse("smsto: " + Uri.encode("+91 7902098062")));
+                iMsg.putExtra("sms_body","Message to Send");
+                startActivity(iMsg);
+
+            }
+        });
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iEmail = new Intent(Intent.ACTION_SENDTO);
+                iEmail.setType("message/rfc822");
+                iEmail.putExtra(Intent.EXTRA_EMAIL,new String[]{"dixitk488@gmail.com"});
+                iEmail.putExtra(Intent.EXTRA_SUBJECT,"This is the Subject");
+                iEmail.putExtra(Intent.EXTRA_TEXT,"This is the Message to send.");
+                startActivity(iEmail);
+
+            }
+        });
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iShare = new Intent(Intent.ACTION_SEND);
+                iShare.setType("text/pain");
+                iShare.putExtra(Intent.EXTRA_TEXT,"kuldeeppanditg1234.wixsite.com/mysite");
+                startActivity(Intent.createChooser(iShare,"share via.."));
+            }
         });
     }
 }
