29bff57b4816e44c3e22489a8f229ccef260882e
Kuldeep Dixit <dixitk488@gmail.com>
2025-04-20 11:37:25 +0530

    Implementing shared Preferences

--- SharedPrefernce/app/src/main/java/com/example/sharedprefernce/LoginWindow.java
+++ SharedPrefernce/app/src/main/java/com/example/sharedprefernce/LoginWindow.java
@@ -1,6 +1,13 @@
 package com.example.sharedprefernce;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginWindow extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,31 @@ public class LoginWindow extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btn = findViewById(R.id.login);
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+        TextView show = findViewById(R.id.showMessage);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(email.getText().toString().equals("") || password.getText().toString().equals("")){
+                    show.setText("Please Enter a Valid Credentials");
+                }
+                else{
+                    SharedPreferences pref = getSharedPreferences("login",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = pref.edit();
+
+                    editor.putBoolean("flag",true);
+                    editor.apply();
+
+                    Intent intent = new Intent(LoginWindow.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
         });
     }
 }
--- SharedPrefernce/app/src/main/java/com/example/sharedprefernce/MainActivity.java
+++ SharedPrefernce/app/src/main/java/com/example/sharedprefernce/MainActivity.java
@@ -1,46 +1,45 @@
 package com.example.sharedprefernce;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
+import android.os.Handler;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
-    @SuppressLint("MissingInflatedId")
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btn = findViewById(R.id.btnlogOut);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                SharedPreferences pref = getSharedPreferences("login",MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
+            public void run() {
 
-                editor.putBoolean("flag",false);
-                editor.apply();
+                SharedPreferences pref = getSharedPreferences("login", MODE_PRIVATE);
+                Boolean check = pref.getBoolean("flag",false);
 
-                Intent intent = new Intent(HomeActivity.this, LoginWindow.class);
+                Intent intent;
+
+                if(check){
+                    intent = new Intent(MainActivity.this, HomeActivity.class);
+                }
+                else{
+                    intent = new Intent(MainActivity.this, LoginWindow.class);
+                }
                 startActivity(intent);
                 finish();
             }
-        });
-
+        },2000);
     }
 }
